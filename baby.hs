35b3832ac32a57b24cfea4fd2fe105612894261b
Michael Sarfati <michael.sarfati@utoronto.ca>
2016-12-28 17:39:05 -0500

    list comps for nouns and adjectives

--- baby.hs
+++ baby.hs
@@ -12,6 +12,19 @@ doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
 
 sqrMe x = x * x
 
+-- List comprehensions
 powersOf2 = [2**x | x <- [1..15]]
 filteredDoubleEven = [x*2 | x <- [1..10], x*2 >= 12]
 remainderDividedBy7 = [x | x <- [50..100], x `mod` 7 == 3]
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+boomBangResult = boomBangs [7..13]
+
+listCompExceptFew = [x | x <- [10..20], x /=13, x/=15, x/=19]
+
+listCompDiffLists = [x+y | x <- [1,2,3], y <- [10,100,1000]]
+
+-- List comprehension of words
+nouns = ["hobo","frog","pope"]
+adjectives = ["lazy", "grouchy", "scheming"]
+wordStew = [adjective ++ " " ++ noun | adjective <- adjectives, noun <- nouns]
