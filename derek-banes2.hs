5f1ae4410444ee4074f25032b8f73acca48efdc5
Michael Sarfati <michael.sarfati@utoronto.ca>
2016-12-27 20:17:19 -0500

    recursion +

--- derek-banes2.hs
+++ derek-banes2.hs
@@ -86,3 +86,9 @@ listTimes4 = map times4 [1,2,3,4,5]
 multBy4 :: [Int] -> [Int]
 multBy4 [] = []
 multBy4 (x:xs) = times4 x: multBy4 xs
+
+-- More recursion
+areStringsEq :: [Char] -> [Char] -> Bool
+areStringsEq [] [] = True
+areStringsEq (x:xs) (y:ys) = x == y && areStringsEq xs ys
+areStringsEq _ _ = False
