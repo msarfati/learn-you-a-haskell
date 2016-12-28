92299f55b3fe5943f41941691ab5ae0670e3563b
Michael Sarfati <michael.sarfati@utoronto.ca>
2016-12-28 17:27:59 -0500

    list comprehensions

--- baby.hs
+++ baby.hs
@@ -11,3 +11,7 @@ doubleSmallNumber x = if x > 100
 doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
 
 sqrMe x = x * x
+
+powersOf2 = [2**x | x <- [1..15]]
+filteredDoubleEven = [x*2 | x <- [1..10], x*2 >= 12]
+remainderDividedBy7 = [x | x <- [50..100], x `mod` 7 == 3]
