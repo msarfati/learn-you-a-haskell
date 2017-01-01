8b504fdead415b9b26458a99da8cf58e77877dd3
Michael Sarfati <michael.sarfati@utoronto.ca>
2017-01-01 13:00:55 -0500

    replicate, take and reverse

--- ch4.hs
+++ ch4.hs
@@ -17,3 +17,21 @@ minimum' :: (Ord a) => [a] -> a
 minimum' [] = error "Minimum cannot be of an empty list."
 minimum' [x] = x
 minimum' (x:xs) = min x (minimum' xs)
+
+-- Replicate
+replicate' :: Int -> a -> [a]
+replicate' n x
+    | n <= 0   = []
+    | otherwise= x : replicate' (n-1) x
+
+-- Take
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0    = []
+take' _ []      = []
+take' n (x:xs)  = x : take' (n-1) xs
+
+-- Reverse
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse; (x:xs) = reverse' xs ++ [x]
