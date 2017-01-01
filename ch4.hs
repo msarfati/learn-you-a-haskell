87d3162c403b4353f422c107a3a8452dbb857b70
Michael Sarfati <michael.sarfati@utoronto.ca>
2017-01-01 03:13:19 -0500

    maximum recursion

--- ch4.hs
+++ ch4.hs
@@ -1,7 +1,19 @@
 -- Chapter 4: Recursion
 --
+fib :: Int -> Int
 fib 0 = 0
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
 
 fibSequence = [fib n | n <- [1..15]]
+
+-- Maximum
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "Maximum cannot be of an empty list."
+maximum' [x] = x
+maximum' (x:xs) = max x (maximum' xs)
+
+minimum' :: (Ord a) => [a] -> a
+minimum' [] = error "Minimum cannot be of an empty list."
+minimum' [x] = x
+minimum' (x:xs) = min x (minimum' xs)
---
+++ happylearninghaskellutorial/ch5.hs
@@ -0,0 +1,5 @@
+-- Chapter 5
+lambdaTrue = (\x -> True) False
+
+magicCoin :: Bool -> Bool
+magicCoin = \_ -> True
