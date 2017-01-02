f2dfe049597a4abd0a3a5a7348c581011ec1ac2c
Michael Sarfati <michael.sarfati@utoronto.ca>
2017-01-02 11:27:40 -0500

    magic coins

---
+++ ch5.hs
@@ -0,0 +1,4 @@
+-- Chapter 5 - Higher Order Functions
+
+
+
--- happylearninghaskellutorial/ch5.hs
+++ happylearninghaskellutorial/ch5.hs
@@ -2,4 +2,16 @@
 lambdaTrue = (\x -> True) False
 
 magicCoin :: Bool -> Bool
-magicCoin = \_ -> True
+magicCoin = (\_ -> True)
+
+newCoin :: Bool
+newCoin = magicCoin False
+
+newCoinAgain :: Bool
+newCoinAgain = magicCoin newCoin
+
+newCoinAgain' :: Bool
+newCoinAgain' = magicCoin (magicCoin False)
+
+magicCoin' :: Bool -> Bool
+magicCoin' _ = True
