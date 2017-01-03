7d39cc1624b5cc9c0233e479a10e081aec63d7f9
Michael Sarfati <michael.sarfati@utoronto.ca>
2017-01-02 19:12:11 -0500

    currying examples

--- happylearninghaskellutorial/ch5.hs
+++ happylearninghaskellutorial/ch5.hs
@@ -15,3 +15,42 @@ newCoinAgain' = magicCoin (magicCoin False)
 
 magicCoin' :: Bool -> Bool
 magicCoin' _ = True
+
+not' :: Bool -> Bool
+not' False = True
+not' True = False
+
+-- Functions returning functions
+
+magicBool :: Bool -> (Bool -> Bool)
+magicBool = \_  -> magicCoin
+magicBoolCoin = magicBool False False
+
+magicBool'1 :: Bool -> Bool -> Bool
+magicBool'1 = \_ -> \_ -> True
+
+magicBool'2 :: Bool -> Bool -> Bool
+magicBool'2 = \_ _ -> True
+
+magicBool'' :: Bool -> Bool -> Bool
+magicBool'' _ _ = True
+
+eitherTrue :: Bool -> Bool -> Bool
+eitherTrue False False = True
+eitherTrue _     _     = True
+
+-- The process of making a function return a function is called 'currying'
+--
+
+plus :: Int -> Int -> Int
+plus x y = x + y
+
+plus' :: Int -> Int -> Int
+plus' = \x -> \y -> x + y
+
+increment' :: Int -> Int
+increment' = (\x -> \y -> x + y) 1
+
+additionResult :: Int
+additionResult = plus 100 25
+
